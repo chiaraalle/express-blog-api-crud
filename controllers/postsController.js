78641d04e15eaf5a245442aea62ac79f4c1471fc
chiaralle <chiaraallegrino@gmail.com>
2025-02-12 18:28:10 +0100

    add:fine esercizio

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -7,7 +7,27 @@ function index(req, res) {
 }
 // show
 function show(req, res) {
-    res.send('Dettagli del post' + req.params.id);
+    //res.send('Dettagli del post' + req.params.id);
+    // recupero il parametro dinamico che rappresenta l'id del post e lo converto in numero.
+    const id = parseInt(req.params.id) 
+     // cerco il post tramite id quindi utilizzo il metodo find
+     const post = posts.find(post => post.id === id);
+     // Restituisco il risultato sotto forma di JSON
+     res.json(post);
+    
+    /*cotrollo se l'id inserito è presente o meno 
+    if (post) {
+        // cerco il post tramite id quindi utilizzo il metodo find
+        const post = posts.find(post => post.id === id);
+        // Restituisco il risultato sotto forma di JSON
+        res.json(post);
+      } else {
+        res.status(404);
+        return res.json({
+          error: "Not Found",
+          message: "Post non trovato"
+        });
+      }*/
 }
 // store
 function store(req, res) {
@@ -23,7 +43,25 @@ function modify(req, res) {
 };
 // destroy
 function destroy(req, res) {
-    res.send('Eliminazione del post ' + req.params.id);
+    //res.send('Eliminazione del post ' + req.params.id);
+
+    const id = parseInt(req.params.id)
+    const post = posts.find(post => post.id === id);
+    // Piccolo controllo
+    if (!post) {
+        res.status(404);
+        return res.json({
+        status: 404,
+        error: "Not Found",
+        message: "Post non trovato"
+        })
+    }
+    // Rimuovo il post utilizzando splice e indexof()
+    posts.splice(posts.indexOf(post), 1);
+    // Restituiamo lo status corretto
+    res.sendStatus(204)
+    
+    console.log(posts)
 }
 
 module.exports = { index, show, store, update, modify, destroy }
