796a1d1ef24895ef7949bcac1f4fb35667f1ff90
chiaralle <chiaraallegrino@gmail.com>
2025-02-15 15:36:12 +0100

    add: milestone 1

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -15,22 +15,23 @@ function show(req, res) {
      // Restituisco il risultato sotto forma di JSON
      res.json(post);
     
-    /*cotrollo se l'id inserito è presente o meno 
-    if (post) {
-        // cerco il post tramite id quindi utilizzo il metodo find
-        const post = posts.find(post => post.id === id);
-        // Restituisco il risultato sotto forma di JSON
-        res.json(post);
-      } else {
-        res.status(404);
-        return res.json({
-          error: "Not Found",
-          message: "Post non trovato"
-        });
-      }*/
+    //cotrollo se l'id inserito è presente o meno 
+    if (!post) {
+        res.status(404)
+
+       return res.json(
+           {
+               status: 404,
+               error: "Not Found",
+               message: "Post non trovato"
+           }
+       )
+   }
+    
 }
 // store
 function store(req, res) {
+    console.log(req.body);
     res.send('Creazione nuovo post');
 }
 // update
@@ -49,12 +50,15 @@ function destroy(req, res) {
     const post = posts.find(post => post.id === id);
     // Piccolo controllo
     if (!post) {
-        res.status(404);
-        return res.json({
-        status: 404,
-        error: "Not Found",
-        message: "Post non trovato"
-        })
+         res.status(404)
+
+        return res.json(
+            {
+                status: 404,
+                error: "Not Found",
+                message: "Post non trovato"
+            }
+        )
     }
     // Rimuovo il post utilizzando splice e indexof()
     posts.splice(posts.indexOf(post), 1);
